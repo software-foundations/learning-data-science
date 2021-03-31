81dd8b9d93978da8ac4908234c7bdafcd0f11dd4
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-03-31 15:33:01 -0300

    (datavisualization/matplotlib/matplotlib.py, at /) -> 42: Matplotlib - part 1

--- data_visualization/matplotlib/matplotlib.py
+++ data_visualization/matplotlib/matplotlib.py
@@ -10,13 +10,15 @@ from matplotlib.font_manager import FontProperties
 import numpy as np
 import pandas as pd
 
+######################
+# Matplotlib - part 01
+######################
 
 x = np.linspace(0, 5, 11)
 
 y = x * x
 
-
-# Functional
+# -> Functional
 
 plt.plot(x, y, color='r')
 
@@ -29,14 +31,14 @@ plt.title('Title')
 plt.show()
 
 
-# Subplots
+# -> Subplots
 
 plt.subplots(nrows=1, ncols=2)
 
 plt.show()
 
 
-# Subplot 02
+# -> Subplot 02
 
 plt.subplot(1, 2, 1)
 
@@ -53,7 +55,7 @@ plt.plot(x, y, color='g')
 plt.show()
 
 
-# Figure and axes
+# -> Figure and its axes
 
 fig = plt.figure()
 
@@ -65,3 +67,42 @@ rect = [left, bottom, width, height]
 
 axes = fig.add_axes(rect=rect)
 
+axes.plot(x, y)
+
+axes.set_xlabel('Axis x')
+
+axes.set_ylabel('Axis y')
+
+axes.set_title('Title')
+
+plt.show()
+
+
+#-> Figure and its axes
+
+
+# figure
+
+fig = plt.figure()
+
+# axes
+
+axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+axes2 = fig.add_axes([0.2, 0.5, 0.3, 0.3])
+
+# axes1
+
+axes1.plot(x, y)
+
+axes1.set_xlabel('Axis x')
+
+axes1.set_ylabel('Axis y')
+
+axes1.set_title('Title')
+
+# axes 2
+
+axes2.plot(x, y, color='r')
+
+plt.show()
